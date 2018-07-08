96e23d2cfa0a95cffdd2bc228efabb1d8e7cb3e5
Debashish <dmajumd2@binghamton.edu>
2018-07-08 12:58:03 -0400

    Created AngularPost Project -Initialization with registration and navbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RegistrationComponent } from './auth/registration/registration.component';
+import { NavbarComponent } from './auth/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrationComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
